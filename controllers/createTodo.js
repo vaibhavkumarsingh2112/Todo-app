27a68014abf10020d5b1abfbdc4e9677501234cc
vaibhav <vaibhavisinghak47@gmail.com>
2024-11-17 11:08:38 +0530

    Initial commit

---
+++ controllers/createTodo.js
@@ -0,0 +1,31 @@
+
+const Todo = require("../models/Todo");
+
+
+
+exports.createTodo = async(req,res) => {
+    try {
+            
+            const {title,description} = req.body;
+          
+            const response = await Todo.create({title,description});
+         
+            res.status(200).json(
+                {
+                    success:true,
+                    data:response,
+                    message:'Entry Created Successfully'
+                }
+            );
+    }
+    catch(err) {
+        console.error(err);
+        console.log(err);
+        res.status(500)
+        .json({
+            success:false,
+            data:"internal server error",
+            message:err.message,
+        })
+    }
+}
---
+++ controllers/getTodo.js
@@ -0,0 +1,62 @@
+
+const Todo = require("../models/Todo");
+
+
+
+exports.getTodo = async(req,res) => {
+    try {
+            
+            const todos = await Todo.find({});
+
+            res.status(200)
+            .json({
+                success:true,
+                data:todos,
+                message:"Entire Todo Data is fetched",
+            });
+    }
+    catch(err) {
+        console.error(err);
+        res.status(500)
+        .json({
+            success:false,
+            error:err.message,
+            message:'Server Error',
+        });
+       
+    }
+}
+
+
+exports.getTodoById = async(req, res) => {
+    try {
+  
+       const id = req.params.id;
+       const todo = await Todo.findById( {_id: id})
+
+
+       if(!todo) {
+        return res.status(404).json({
+            success:false,
+            message:"No Data Found woth Given Id",
+        })
+       }
+      
+       res.status(200).json({
+        success:true,
+        data:todo,
+        message: `Todo ${id} data successfully fetched`,
+       })
+
+    }
+    catch(err) {
+        console.error(err);
+        res.status(500)
+        .json({
+            success:false,
+            error:err.message,
+            message:'Server Error',
+        });
+    
+    }
+}
